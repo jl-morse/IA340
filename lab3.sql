4a7c619033fb0f967951077cb43e17e8bc3d85fb
jl-morse <143039101+jl-morse@users.noreply.github.com>
2023-09-13 17:36:01 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -1,2 +1,25 @@
+--q2.1
 insert into student(s_email, s_name, major)
 values ('s1@jmu.edu', 's1', 'IA'), ('s2@jmu.edu', 's2', 'IA'), ('s3@jmu.edu', 's3', 'ISAT'), ('s4@jmu.edu', 's4', 'ISAT')
+
+--q2.2
+insert into public.professor(p_email,p_name,office)
+values ('pl@jmu.edu','p1','o1'),
+		('p2@jmu.edu','p2','o2'),
+		('p3@jmu.edu','p3','o3')
+
+--q2.3
+insert into course(c_number,c_name,room,p_email)
+values ('c1','postgresql','r1','p1@jmu.edu'),
+		('c2','postgresql','r2','p2@jmu.edu'),
+		('c3','postgresql','r3','p3@jmu.edu')
+
+--q2.4
+insert into enroll(s_email,c_number)
+values
+('s1@jmu.edu', 'c1'),
+('s2@jmu.edu', 'c1'),
+('s3@jmu.edu', 'c1'),
+('s4@jmu.edu', 'c2'),
+('s2@jmu.edu', 'c3'),
+('s3@jmu.edu', 'c3')
